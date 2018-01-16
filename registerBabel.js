d0cbaf810560d450181f514cc32ef490a5c4d912
Nathaniel Tucker <me@ntucker.me>
2018-01-15 17:23:59 -0800

    [shim] Add requestIdleCallback shim to registerBabel

--- registerBabel.js
+++ registerBabel.js
@@ -3,3 +3,5 @@ require('babel-core/register')({
   ignore: /node_modules/
 })
 require('babel-polyfill')
+global.requestIdleCallback = require('ric-shim')
+global.cancelIdleCallback = global.requestIdleCallback.cancelIdleCallback
