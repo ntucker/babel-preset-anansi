9ce2b7aa8de3501d81c045c4f9f23eeb99cf93c4
Nathaniel Tucker <me@ntucker.me>
2018-09-23 18:40:45 -0700

    [pkg] All defaults

--- index.js
+++ index.js
@@ -8,57 +8,57 @@ function buildPreset(context, options = {}) {
   )
   const preset = {
     presets: [
-      [require('@babel/preset-react'), { development: env !== 'production' }],
+      [require('@babel/preset-react').default, { development: env !== 'production' }],
     ],
     plugins: [
       require('babel-plugin-react-require').default,
       require('babel-plugin-root-import').default,
       // stage 3, but must come before class-properties
       [
-        require('@babel/plugin-proposal-decorators'),
+        require('@babel/plugin-proposal-decorators').default,
         { decoratorsBeforeExport: true },
       ],
       //stage 1
       require('@babel/plugin-proposal-export-default-from').default,
-      require('@babel/plugin-proposal-export-namespace-from'),
-      require('@babel/plugin-proposal-optional-chaining'),
-      require('@babel/plugin-proposal-nullish-coalescing-operator'),
+      require('@babel/plugin-proposal-export-namespace-from').default,
+      require('@babel/plugin-proposal-optional-chaining').default,
+      require('@babel/plugin-proposal-nullish-coalescing-operator').default,
       //stage 2
-      require('@babel/plugin-proposal-class-properties'),
+      require('@babel/plugin-proposal-class-properties').default,
       //stage 3
-      require('@babel/plugin-syntax-dynamic-import'),
+      require('@babel/plugin-syntax-dynamic-import').default,
     ],
   }
   switch (options.typing) {
     case 'flow':
-      preset.presets.push(require('@babel/preset-flow'))
+      preset.presets.push(require('@babel/preset-flow').default)
       break
     case 'typescript':
-      preset.presets.push(require('@babel/preset-typescript'))
+      preset.presets.push(require('@babel/preset-typescript').default)
       break
   }
   switch (env) {
     case 'production':
       preset.plugins.unshift(
-        require('@babel/plugin-transform-react-inline-elements'),
-        require('@babel/plugin-transform-react-constant-elements'),
+        require('@babel/plugin-transform-react-inline-elements').default,
+        require('@babel/plugin-transform-react-constant-elements').default,
         require('babel-plugin-transform-react-remove-prop-types').default,
       )
       break
     case 'development':
-      preset.plugins.unshift(require('react-hot-loader/babel'))
+      preset.plugins.unshift(require('react-hot-loader/babel').default)
       break
   }
 
   if (!options.runInNode && env === 'production') {
     preset.plugins.unshift(
       require('babel-plugin-ramda').default,
-      require('babel-plugin-lodash'),
+      require('babel-plugin-lodash').default,
     )
   }
 
   if (env === 'test' || options.runInNode) {
-    preset.plugins.push(require('@babel/plugin-proposal-object-rest-spread'))
+    preset.plugins.push(require('@babel/plugin-proposal-object-rest-spread').default)
     preset.presets.unshift([
       require('@babel/preset-env').default,
       {
@@ -67,7 +67,7 @@ function buildPreset(context, options = {}) {
         },
       },
     ])
-    preset.plugins.unshift(require('babel-plugin-dynamic-import-node'))
+    preset.plugins.unshift(require('babel-plugin-dynamic-import-node').default)
   } else {
     let targets = options.targets
     if (!targets) {
@@ -101,8 +101,9 @@ function buildPreset(context, options = {}) {
     ])
   }
   if (options.minify && env === 'production') {
-    preset.presets.unshift(require('babel-minify'))
+    preset.presets.unshift(require('babel-minify').default)
   }
+  console.log(preset)
   return preset
 }
 
